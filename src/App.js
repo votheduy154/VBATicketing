b93c9adefe03080c12dae996a9b2c97827f8d371
Duy Nguyen <duynguyen@Duys-MacPro.local>
2017-12-25 01:15:38 +0700

    add js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,5 @@ Navigation.startSingleScreenApp({
   screen: {
     screen: 'RNBoot.HomeScreen', // unique ID registered with Navigation.registerScreen
     title: 'Welcome', // title of the screen as appears in the nav bar (optional)
-    navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
-    navigatorButtons: {}, // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
   },
 });
